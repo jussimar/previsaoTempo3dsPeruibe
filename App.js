d78d8bd7ee236675d5b36ce01b9bcc09d9dcc407
jussimar <jussimar@jussimarleal.com.br>
2022-08-04 09:32:19 -0300

    criando layout basico

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} 
+from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.bloco}>
+        <Text style={styles.titulo}>Previsão do Tempo</Text>
+      </View>
+      <View style={styles.blocoGeral}>
+        <Text style={styles.label}>Digite sua Cidade: </Text>
+        <TextInput 
+          placeholder='sua cidade ...'
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.blocoGeral}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +28,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bloco:{
+    alignItems:'center',
+    marginBottom:20,
+    marginTop:20
+  },
+  titulo:{
+    fontSize:30
+  },
+  label:{
+    fontSize:20,
+    marginLeft:'10%'
+  },
+  input:{
+    marginLeft:'10%',
+    borderBottomWidth:2,
+    marginRight:'10%',
+    fontSize:20
+  },
+  botao:{
+    marginLeft:'10%',
+    marginRight:'10%',
+    backgroundColor:'#000',
+  },
+  textoBotao:{
+    color:'#FFF',
+    fontSize:20,
+    textAlign:'center'
+  },
+  blocoGeral:{
+    marginTop:20
+  }
 });
